fbea4d09bda264b4c0a7d8f982fc8f7e44d8c349
Eugene <maskot2002@yandex.ru>
2022-01-15 09:33:38 +0300

    update

--- AnimeShutdownGUI-fix/src/main/java/com/eumll/animeshutdownguifix/main.java
+++ AnimeShutdownGUI-fix/src/main/java/com/eumll/animeshutdownguifix/main.java
@@ -1,6 +1,7 @@
 package com.eumll.animeshutdownguifix;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Pos;
@@ -17,6 +18,7 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 public class main extends Application {
+
     @Override
     public void start(Stage stage) {
         stage.setTitle("Anime Shutdown GUI");
@@ -29,7 +31,6 @@ public class main extends Application {
         ss.setId("SS");
 
         Text timeText = new Text();
-
 
         TextField chas = new TextField();
         TextField minuts = new TextField();
@@ -47,38 +48,22 @@ public class main extends Application {
         minuts.setPrefWidth(80);
         seconds.setPrefWidth(80);
 
+        timeText.setText("Жду приказа :3");
+        timeText.setId("timeTEXT");
         Button button = new Button("Start");
         button.setId("start");
         button.setPrefWidth(60);
+
+        Button button1 = new Button("Stop");
+        button1.setId("start");
+        button1.setPrefWidth(60);
+
+        Timer timer = new Timer("Timer");
+
         button.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent actionEvent) {
-                TimerTask task = new TimerTask() {
-                    @Override
-                    public void run() {
-                        String shitDownComm = null;
-                        String OSN = System.getProperty("os.name");
-
-                        if (OSN.startsWith("Win")) {
-                            shitDownComm = "shutdown.exe -s -t 0";
-                        }
-                        else if (OSN.startsWith("Linux") || OSN.startsWith("Mac")) {
-                            shitDownComm= "shutdown -h now";
-                        }
-                        else {
-                            System.err.println("Shutdown unsupported operating system ...");
-                        }
-
-                        try {
-                            Runtime.getRuntime().exec(shitDownComm);
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                        System.exit(0);
-                    }
-                };
-                Timer timer = new Timer("Timer");
-
+                button.setDisable(true);
                 String textChas = chas.getText();
                 int digChas = Integer.parseInt(textChas);
                 String textMin = minuts.getText();
@@ -86,40 +71,34 @@ public class main extends Application {
                 String textSec = seconds.getText();
                 int digSec = Integer.parseInt(textSec);
 
-                int Chas = 0;
-                int Min = 0;
-                int Sec = 0;
-
-                if(digChas != 0){
-                    Chas = digChas * 3600000;
-                }
-                else if(digMin != 0){
-                    Min = digMin * 60000;
-                }
-                else if(digSec != 0){
-                    Sec = digSec * 1000;
-                }
-
-                int sum = Chas + Min + Sec;
-
-
-
-                for(int i = sum; i != 0; i--){
-
-                    long hour = i / 3600, min = i / 60 % 60, sec = i / 1 % 60;
-                    String time = String.format("%02d:%02d:%02d", hour, min, sec);
-                    timeText.setText(time);
-                    try {
-                        Thread.sleep(1000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
+                TimerTask timerTask = new TimerTask() {
+                    int sumSec = (digChas * 3600) + (digMin * 60) + digSec;
+                    @Override
+                    public void run() {
+                        sumSec = sumSec - 1;
+                        timeText.setText(sumSec + " секунд");
+                        if(sumSec == 0){
+                            OsCom();
+                        }
                     }
-                }
-
-                timer.schedule(task, sum);
+                };
+                timer.scheduleAtFixedRate(timerTask, 0, 1000 );
             }
         });
 
+        button1.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                button.setDisable(false);
+                timer.cancel();
+                stage.close();
+                Platform.runLater( () -> new main().start( new Stage() ) );
+            }
+        });
+
+
+        Text n1 = new Text("");
+        Text n12 = new Text("");
 
         GridPane grid = new GridPane();
         grid.setAlignment(Pos.CENTER);
@@ -134,12 +113,56 @@ public class main extends Application {
         grid.add(chas, 0,1);
         grid.add(minuts, 1, 1);
         grid.add(seconds, 2, 1);
-        grid.add(button, 1, 2);
-        grid.add(timeText,3, 1);
+        grid.add(button, 3, 1);
+        grid.add(button1,3,2);
+        grid.add(timeText,1, 2);
         stage.setScene(scene);
         stage.show();
     }
 
+    public void OsCom(){
+        String shitDownComm = null;
+        String OSN = System.getProperty("os.name");
+
+        if (OSN.startsWith("Win")) {
+            shitDownComm = "shutdown.exe -s -t 0";
+        }
+        else if (OSN.startsWith("Linux") || OSN.startsWith("Mac")) {
+            shitDownComm= "shutdown -h now";
+        }
+        else {
+            System.err.println("Shutdown unsupported operating system ...");
+        }
+
+        try {
+            Runtime.getRuntime().exec(shitDownComm);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.exit(0);
+    }
+
+    /*public void TimerGUI(){
+        int tSumSec = (digChas * 3600) + (digMin * 60) + digSec;
+        TimerTask countTimer = new TimerTask() {
+            @Override
+            public void run() {
+                for(int i = tSumSec; i != 0; i--) {
+                    long hour = i / 3600, min = i / 60 % 60, sec = i / 1 % 60;
+                    String time = String.format("%02d:%02d:%02d", hour, min, sec);
+                    timeText.setText(time);
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        };
+        Timer timerCountTimer = new Timer("Timer Counter");
+        timerCountTimer.schedule(countTimer, tSumSec);
+    }
+*/
     public static void main(String[] args) {
         launch(args);
     }
